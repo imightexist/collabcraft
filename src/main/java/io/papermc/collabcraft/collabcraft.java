de3dc98d3367b5027aa85b195473a9e45b10f324
imightexist <94249457+imightexist@users.noreply.github.com>
2022-11-24 10:40:52 -0600

    Update collabcraft.java

--- src/main/java/io/papermc/collabcraft/collabcraft.java
+++ src/main/java/io/papermc/collabcraft/collabcraft.java
@@ -17,65 +17,75 @@ import java.lang.*;
 public class collabcraft extends JavaPlugin implements Listener,CommandExecutor {
     private Player controller;
     private ArrayList<Player> waiting = new ArrayList<Player>();
-    private int time = 30; //in seconds
-    private BossBar turnTime = Bukkit.createBossBar("PlayerNameHere's turn",BarColor.BLUE,BarStyle.SOLID);
+    private long time = 300L; //ms
+    private BossBar turnTime = Bukkit.createBossBar("No one is controlling",BarColor.BLUE,BarStyle.SOLID);
     private PlayerInventory inventory = null;
     private Location location = null;
     private Location spawn = null;
+    private float exp = 0;
     private boolean joined = false;
-    private Runnable createRunnable(Player p){
-        Runnable r = new Runnable(){
+    private double progress = 0.0;
+    private double health = 0.0;
+    private TimerTask createRunnable(Player p){
+        turnTime.setTitle(p.getName() + "'s turn");
+        turnTime.setProgress(0.0);
+        TimerTask r = new TimerTask(){
             public void run(){
-                String name = p.getName();
-                double progress = 0.0;
-                turnTime.setTitle(name + "'s turn");
-                turnTime.setProgress(0.0);
-                while (waiting.get(0) == p){
-                    if (progress >= 100.0){
-                        waiting.remove(0);
+                if (waiting.get(0) == p){
+                    progress = progress + 0.01;
+                    turnTime.setProgress(progress);
+                    if (progress >= 1.0){
+                        p.setGameMode(GameMode.SPECTATOR);
+                        waiting.remove(p);
                         inventory = p.getInventory();
                         location = p.getLocation();
                         spawn = p.getBedSpawnLocation();
+                        exp = p.getExp();
+                        health = p.getHealth();
                         if (!(waiting.size() == 0)){
                             Player author = waiting.get(0);
-                            p.setGameMode(GameMode.SPECTATOR);
                             author.getInventory().setArmorContents(p.getInventory().getArmorContents());
                             author.getInventory().setExtraContents(p.getInventory().getExtraContents());
                             author.teleport(p.getLocation());
+                            author.setExp(exp);
                             author.setBedSpawnLocation(p.getBedSpawnLocation());
                             author.setGameMode(GameMode.SURVIVAL);
-                            turnThread = new Thread(createRunnable(author));
-                            turnThread.start();
+                            progress = 0.0;
+                            turnThread = new Timer();
+                            turnThread.scheduleAtFixedRate(createRunnable(author),time,time);
                         }
-                    }
-                    progress = progress + 1.0;
-                    turnTime.setProgress(progress);
-                    try{
-                        Thread.sleep(time*10);
-                    }catch(Exception e){
-
                     }
                 }
             }
         };
         return r;
     }
-    private Thread turnThread;
+    private Timer turnThread;
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
         turnTime.setVisible(true);
+        turnTime.setProgress(0.0);
     }
-    public void onPlayerJoin(Player p){
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent p){
         if (joined == false){
-            inventory = p.getInventory();
-            location = p.getLocation();
-            spawn = p.getBedSpawnLocation();
+            inventory = p.getPlayer().getInventory();
+            location = p.getPlayer().getLocation();
+            spawn = p.getPlayer().getBedSpawnLocation();
+            health = p.getPlayer().getHealth();
+            joined = true;
         }
-        p.setGameMode(GameMode.SPECTATOR);
+        p.getPlayer().setGameMode(GameMode.SPECTATOR);
+        turnTime.addPlayer(p.getPlayer());
     }
-    public void onPlayerQuit(Player p){
-        if (waiting.contains(p)){
-            waiting.remove(p);
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent p){
+        if (waiting.get(0) == p.getPlayer()){
+            turnTime.setTitle("No one is controlling");
+            turnTime.setProgress(0.0);
+        }
+        if (waiting.contains(p.getPlayer())){
+            waiting.remove(p.getPlayer());
         }
     }
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
@@ -86,24 +96,35 @@ public class collabcraft extends JavaPlugin implements Listener,CommandExecutor 
                 inventory = controller.getInventory();
                 location = controller.getLocation();
                 spawn = controller.getBedSpawnLocation();
+                health = controller.getHealth();
+                exp = controller.getExp();
                 controller.setGameMode(GameMode.SPECTATOR);
                 author.getInventory().setArmorContents(controller.getInventory().getArmorContents());
                 author.getInventory().setExtraContents(controller.getInventory().getExtraContents());
                 author.teleport(controller.getLocation());
+                author.setExp(exp);
+                author.setHealth(health);
                 author.setBedSpawnLocation(spawn);
                 controller = author;
                 controller.setGameMode(GameMode.SURVIVAL);
-                turnThread = new Thread(createRunnable(controller));
-                turnThread.start();
+                progress = 0.0;
+                turnThread = new Timer();
+                turnThread.scheduleAtFixedRate(createRunnable(author),time,time);
                 return true;
             }else if (cmd.getName().equalsIgnoreCase("endturn")){
                 if (waiting.contains(author)){
-                    if (waiting.get(0) == author){
+                    if (waiting.size() == 1){
                         inventory = author.getInventory();
                         location = author.getLocation();
                         spawn = author.getBedSpawnLocation();
+                        health = author.getHealth();
+                        exp = author.getExp();
+                        turnTime.setTitle("No one is controlling");
+                        progress = 0.0;
+                        turnTime.setProgress(0.0);
                     }
                     waiting.remove(author);
+                    author.setGameMode(GameMode.SPECTATOR);
                     return true;
                 }else{
                     return false;
@@ -112,13 +133,17 @@ public class collabcraft extends JavaPlugin implements Listener,CommandExecutor 
                 if (!(waiting.contains(author))){
                     waiting.add(author);
                     if (waiting.get(0) == author){
+                        controller = author;
                         author.setGameMode(GameMode.SURVIVAL);
                         author.getInventory().setArmorContents(inventory.getArmorContents());
                         author.getInventory().setExtraContents(inventory.getExtraContents());
                         author.teleport(location);
+                        author.setExp(exp);
+                        author.setHealth(health);
                         author.setBedSpawnLocation(spawn);
-                        turnThread = new Thread(createRunnable(author));
-                        turnThread.start();
+                        progress = 0.0;
+                        turnThread = new Timer();
+                        turnThread.scheduleAtFixedRate(createRunnable(author),time,time);
                     }
                     return true;
                 }else{
